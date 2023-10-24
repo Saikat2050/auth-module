e5250bc2470f021a3a3afd6216b4b64713039c4a
Saikat2050 <saikatsantra2050@gmail.com>
2023-10-24 21:13:14 +0530

    lib for db connection

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import eventEmitter from "./logging"
+
+async function connectDB() {
+	const mongoDB: string =
+		process.env.MONGODB_URI || "mongodb://localhost:27017/<database>"
+	try {
+		mongoose.Promise = global.Promise
+		await mongoose.connect(mongoDB)
+		eventEmitter.emit("logging", 'Connected to database')
+	} catch (err) {
+		// @ts-ignore
+		eventEmitter.emit("logging", err.toString())
+		process.exit()
+	}
+}
+
+connectDB()
